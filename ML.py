318b8ffca36a70b75d91647d24e21d07115e345e
poppopp2 <166694558+poppopp2@users.noreply.github.com>
2024-04-25 11:38:56 +0900

    version 1.0

---
+++ ML.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+def run_ml():
+    st.text('자동차 가격 예측하기')
+
+    #1. 예측하기 위해서 유저한테 입력을 받는다.
+    X=['Gender','Age','Annual Salary','Credit Card Debt','Net Wort']
+    st.text('성별을 선택하세요')
+    Gender=st.radio('성별 선택', ['남자','여자'])
+    if Gender== '남자':
+        Gender = 1
+    elif Gender == '여자':
+        Gender = 0
+
+    st.text('나이를 입력하세요.')
+    Age=st.number_input('나이 입력',0,100,value=24)
+
+    st.text('연봉을 입력하세요.')
+    Salary=st.number_input('연봉 입력.',10000,value=50000,step=1000)
+
+    st.text('카드빚을 입력하세요.')
+    debt =st.number_input('카드빚 입력,',0,value=1000,step=1000)
+
+    st.text('자산을 입력하세요.')
+    worth= st.number_input('자산 입력.',5000,value=20000,step=1000)
+
+    st.subheader('버튼을 누르면 예측합니다')
+
+    if st.button('예측하기') :
+        #2. 예측한다.
+        #2-1. 모델이 있어야 한다.
+        regressor=joblib.load('data/regressor.pkl')
+
+        #2-2 유저가 입력한 데이터를,  모델에 예측 할 수 있도록 가공해야 한다,
+        
+        new_data=[Gender,Age,Salary,debt,worth]
+        new_data=np.array(new_data).reshape(1,-1)    
+        
+        
+
+        #2-3 모델의 predict함수로 예측한다.
+        y_pred= regressor.predict(new_data)
+
+
+        #위의 데이터로 예측한 자동차 구매 가능 금액은 6746달러 입니다.
+        y_pred=y_pred[0]
+        y_pred=round(y_pred)
+        y_pred=format(y_pred,',')
+        #predictnum = "{:,.0f}".format(y_pred)
+        st.text(f'자동차 구매 가능 금액은{y_pred}달러 입니다.')
+
+
+
+
+
+        
--- eda.py
+++ eda.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
-
+import seaborn as sb
+import matplotlib.pyplot as plt
+from itertools import combinations
 
 def run_eda():
     st.subheader('탐색적 데이터 분석')
@@ -21,7 +23,46 @@ def run_eda():
 
     #각 컬럼별로 최대/ 최소 값을 보여주는 화면 개발.
     #유저가 컬럼을 선택하면, 해당 커럶의 최대/ 최소 데이터를 보여주도록하자.
+
     st.text('컬럼을 선택하면, 각 컬럼별 최대/ 최소 데이터를 보여드립니다.')
     column_list=['Age',	'Annual Salary', 'Credit Card Debt',	'Net Worth','Car Purchase Amount']
-    st.selectbox('컬럼을 선택 하세요.',column_list)
+    choice_column = st.selectbox('컬럼을 선택 하세요.',column_list)
+
+    st.info(f'선택하신 {choice_column}의 최대 데이터는 다음과 같습니다.')
+    st.dataframe(df.loc[df[choice_column] == df[choice_column].max()])
+
+    st.info(f'선택하신 {choice_column}의 최소 데이터는 다음과 같습니다.')
+    st.dataframe(df.loc[df[choice_column] == df[choice_column].min()])
+
+    st.subheader('상관관계 분석')
+    st.text('컬럼들을 2개 이상 선택하면, 컬럼들의 상관계수를 보여드립니다.')
+
+    corr_column_list=['Age', 'Annual Salary', 'Credit Card Debt',	'Net Worth', 'Car Purchase Amount']
+    selected_columns = st.multiselect('컬럼을 선택하세요.', corr_column_list)
+
+
+    if len(selected_columns) >= 2:
+        #1. 페어플롯을 그린다.
+        #tobo : 1. pairplot 을 다른 라이브러리 이용해서 하는 방법
+        # pairplot 말고, 반복문으로 두 컬럼씩 관계를 차트로 그리는 방법
+        pair_plot = sb.pairplot(data=df, vars=selected_columns)
+        pair_plot.fig.suptitle("corr",y=1.02)
+        st.pyplot(pair_plot)
+
+    
+
+        #.상관계수 보여준다.
+        st.dataframe(df[selected_columns].corr())
+
+        combinations_list = list(combinations(selected_columns, 2))
+        for pair in combinations_list:
+            pair_plot = sb.pairplot(data=df, vars=pair)
+            st.pyplot(pair_plot)
+
+
+    else:
+        st.text('컬럼은 2개이상 선택해야 합니다.')
+
+
+    
     
